4bf124a03fa6acc38bb6ff63c0fd5ba45db1b74a
henryliu <17600047539@163.com>
2022-12-19 00:42:01 +0800

    update

--- src/api/v1/wj/ApiAddWjAccessToken.ts
+++ src/api/v1/wj/ApiAddWjAccessToken.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 import {getRepository} from "typeorm";
 import {Third_party_access_token} from "../../../entity/third_party_access_token";
 import {ReqAddWjAccessToken, ResAddWjAccessToken} from "../../../shared/protocols/v1/wj/PtlAddWjAccessToken";
+import {User_wj_login_code} from "../../../entity/user_wj_login_code";
 
 const appid = 'tpidwOboHH9e';
 const secret = 'T7O5Y4hWBE37Nt95BWpWiAWWMIgYmpy2';
@@ -18,12 +19,31 @@ export default async function (call: ApiCall<ReqAddWjAccessToken, ResAddWjAccess
     });
     const response = await api.get(`/api/oauth2/access_token?appid=${appid}&secret=${secret}&grant_type=client_credential`);
     const data = response.data.data.access_token;
-    const third_party_access_token = new Third_party_access_token()
-    third_party_access_token.app_name = "wj"
-    third_party_access_token.wj_access_token = data
-    await getRepository(Third_party_access_token).save(third_party_access_token);
-    await call.succ({
-        time: time,
-        access_token:data
-    });
+
+    const app_name = "wj"
+    const third_party_access_token = await getRepository(Third_party_access_token).createQueryBuilder("third_party_access_token")
+        .where("third_party_access_token.app_name = :app_name", { app_name })
+        .getOne();
+
+
+    if (third_party_access_token != undefined){
+        third_party_access_token.wj_access_token = data
+        await getRepository(Third_party_access_token).save(third_party_access_token);
+        await call.succ({
+            time: time,
+            access_token:data
+        });
+
+    }else{
+        const third_party_access_token = new Third_party_access_token()
+        third_party_access_token.app_name = "wj"
+        third_party_access_token.wj_access_token = data
+        await getRepository(Third_party_access_token).insert(third_party_access_token);
+        await call.succ({
+            time: time,
+            access_token:data
+        });
+    }
+
+
 }
