cca5023826ff5dbf76f4ecf504c8c8df2334e730
henryliu <17600047539@163.com>
2022-12-20 00:13:19 +0800

    update

--- src/api/v1/teachable/ApiGetTaUser.ts
+++ src/api/v1/teachable/ApiGetTaUser.ts
@@ -13,6 +13,30 @@ export default async function (call: ApiCall<ReqGetTaUser, ResGetTaUser>) {
 
     let time = new Date();
 
+
+    // const loop_get_user_id = async() => {
+    //     sdk.auth('rX28HXFKA2aEA991WtHE5UlmSxMP98rW')
+    //     sdk.listUsers({page: '1'})
+    //         // @ts-ignore
+    //         .then(async ({data}) => {
+    //             const users = data.users
+    //             for (let i = 0; i < users.length; i++) {
+    //                 if (users[i].email == email) {
+    //                     await call.succ({
+    //                         time: time,
+    //                         user_id: (users[i].id).toString()
+    //                     });
+    //                 }
+    //                 if (i == users.length -1){
+    //
+    //                 }
+    //             }
+    //         })
+    //         // @ts-ignore
+    //         .catch(err => console.error(err));
+    // }
+
+
     const email = call.req.user_email
     sdk.auth('rX28HXFKA2aEA991WtHE5UlmSxMP98rW')
     sdk.listUsers({page: '1'})
@@ -25,6 +49,8 @@ export default async function (call: ApiCall<ReqGetTaUser, ResGetTaUser>) {
                         time: time,
                         user_id: (users[i].id).toString()
                     });
+                }
+                if (i == users.length -1){
                 }
             }
         })
