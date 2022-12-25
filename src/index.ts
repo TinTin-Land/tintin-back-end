473156b1e69535ff3663810e930a54ac847c8883
henryliu <17600047539@163.com>
2022-12-26 00:35:09 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -35,7 +35,7 @@ async function init() {
     createConnection({
         type: "postgres",
         host: "localhost",
-        port: 5432,
+        port: 5433,
         username: "postgres",
         password: "123456",
         database: "postgres",
