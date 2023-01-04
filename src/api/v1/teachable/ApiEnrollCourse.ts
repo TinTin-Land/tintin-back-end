76d7b3f0acd1f68efc1c1c47e42d41b5a87d9c7d
henryliu <17600047539@163.com>
2023-01-04 17:46:59 +0800

    update

--- src/api/v1/teachable/ApiEnrollCourse.ts
+++ src/api/v1/teachable/ApiEnrollCourse.ts
@@ -12,23 +12,34 @@ export default async function (call: ApiCall<ReqEnrollCourse, ResEnrollCourse>) 
 
     let time = new Date();
 
+    const user_id = call.req.user_id;
+    const course_id = call.req.course_id;
 
-    const api = axios.create({
-        baseURL: 'https://developers.teachable.com/v1',
-        timeout: 9999,
-        headers: {
-            accept: 'application/json',
-            'content-type': 'application/json',
-            apiKey: 'rX28HXFKA2aEA991WtHE5UlmSxMP98rW'
-        },
-    });
+    const sdk = require('api')('@teachable/v1.1#5zxm7ezlb3yfsh1');
 
-    await api.post('/enroll',{
-        user_id:call.req.user_id,
-        course_id:call.req.course_id
+    sdk.auth('rX28HXFKA2aEA991WtHE5UlmSxMP98rW');
+    // @ts-ignore
+    sdk.enrollUser({user_id, course_id}).then(async ({data}) => {
+        await call.succ({
+            time: time
+        });
     })
+
+
+    // const api = axios.create({
+    //     baseURL: 'https://developers.teachable.com/v1',
+    //     timeout: 9999,
+    //     headers: {
+    //         accept: 'application/json',
+    //         'content-type': 'application/json',
+    //         apiKey: 'rX28HXFKA2aEA991WtHE5UlmSxMP98rW'
+    //     },
+    // });
+    //
+    // await api.post('/enroll',{
+    //     user_id:call.req.user_id,
+    //     course_id:call.req.course_id
+    // })
     // Success
-    await call.succ({
-        time: time
-    });
+
 }
