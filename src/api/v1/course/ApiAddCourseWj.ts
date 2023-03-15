d89578f53e27196f60124ea032f36017417a81b4
Davidxing1 <937104001@qq.com>
2023-03-15 20:42:23 +0800

    ``

--- src/api/v1/course/ApiAddCourseWj.ts
+++ src/api/v1/course/ApiAddCourseWj.ts
@@ -10,10 +10,21 @@ export default async function (call: ApiCall<ReqAddCourseWj, ResAddCourseWj>) {
         return;
     }
     let time = new Date();
+    const course_name =  call.req.course_name
+
+    const course_details = await getRepository(Course_wj_url).createQueryBuilder("course_wj_url")
+        .where("course_wj_url.course_name = :course_name", { course_name })
+        .getOne();
+
     const course_wj_url = new Course_wj_url()
     course_wj_url.course_name = call.req.course_name;
     course_wj_url.course_wj_url_list = call.req.course_wj_url_list;
-    await getRepository(Course_wj_url).insert(course_wj_url);
+    
+    if(course_details == undefined){
+        await getRepository(Course_wj_url).insert(course_wj_url);
+    }else {
+        await getRepository(Course_wj_url).save(course_wj_url);
+    }
     await call.succ({
         time: time,
     });
